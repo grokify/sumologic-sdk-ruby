46641234cf26281ac17c20946afca33770d87ef5
John Wang <johncwang@gmail.com>
2016-02-05 20:20:00 -0800

    update to Ruby 1.9 hash syntax

--- lib/sumologic.rb
+++ lib/sumologic.rb
@@ -12,17 +12,17 @@ module SumoLogic
       @endpoint = endpoint
       @session  = Faraday
       headers   = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
-      @session  = Faraday.new(:url => @endpoint, :headers => headers) do |conn|
+      @session  = Faraday.new(url: @endpoint, headers: headers) do |conn|
         conn.basic_auth(access_id, access_key)
         conn.use      :cookie_jar
         conn.request  :json
-        conn.response :json, :content_type => 'application/json'
+        conn.response :json, content_type: 'application/json'
         conn.adapter  Faraday.default_adapter
       end
     end
 
     def search(query, from_time=nil, to_time=nil, time_zone='UTC')
-      params = {:q => query, :from => from_time, :to => to_time, :tz => time_zone}
+      params = {q: query, from: from_time, to: to_time, tz: time_zone}
       r = @session.get do |req|
         req.url 'logs/search'
         req.params = params
@@ -30,7 +30,7 @@ module SumoLogic
     end
 
     def search_job(query, from_time=nil, to_time=nil, time_zone='UTC')
-      params = {:query => query, :from => from_time, :to => to_time, :timeZone => time_zone}
+      params = {query: query, from: from_time, to: to_time, timeZone: time_zone}
       r = @session.post do |req|
         req.url 'search/jobs'
         req.body = MultiJson.encode(params)
@@ -44,7 +44,7 @@ module SumoLogic
     end
     
     def search_job_records(search_job, limit=nil, offset=0)
-      params = {:limit => limit, :offset => offset}
+      params = {limit: limit, offset: offset}
       r = @session.get do |req|
         req.url 'search/jobs/' + search_job['id'].to_s + '/records'
         req.params = params
@@ -52,7 +52,7 @@ module SumoLogic
     end
 
     def dashboards(monitors=false)
-      params = {:dashboards => monitors}
+      params = {dashboards: monitors}
       r = @session.get do |req|
         req.url 'dashboards'
         req.params = params
